5df4689661ce3e8256fa9e0f7f149da2b26013f4
EmreUte <emreute@gmail.com>
2026-03-05 22:14:34 +0300

    Rough complete of model. ~10M parameters

--- models/model.py
+++ models/model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 from tokenizer import MinecraftTokenizer
 from dataset_loader import MinecraftDataloader
-from transformerblock import MinecraftTransformer
+from transformerblock import TransformerBlock, LayerNorm
 
 with open("vocab.txt", "r", encoding="utf-8") as f:
         words = f.read()
@@ -12,6 +12,7 @@ all_words = words.split('\n')[:-1]
 vocab = {token:integer for integer, token in enumerate(all_words)}
 tokenizer = MinecraftTokenizer(vocab)
 
+"""
 dataloader = MinecraftDataloader(raw_text, 
                                  tokenizer, 
                                  batch_size=4, 
@@ -23,39 +24,20 @@ dataloader = MinecraftDataloader(raw_text,
 data_iter = iter(dataloader)
 first_batch = next(data_iter)
 print(first_batch)
-
-
-
-# Embedding Layer
-vocab_size = len(vocab) + 16 + 1
-output_dim = 256        # can test 128,256,512. increase leads to overfit.
-
-torch.manual_seed(123)
-embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
-
-context_length = 1026 
-pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
-pos_embeddings = pos_embedding_layer(torch.arange(context_length))
-
-input_embeddings = token_embeddings + pos_embeddings
-
-
-
-
-
+"""
 
 # under construction
-MC_GPT_CONFIG = {
+cfg = {
     "vocab_size": 362,
     "context_length": 1026,
     "emb_dim": 256,      # can test 128,256,512. increase leads to overfit.
-    "n_heads": 12,
+    "n_heads": 8,
     "n_layers": 12,
     "drop_rate": 0.1,
     "qkv_bias": False
 }
 
-class Minecraft_GPT(nn.Module):
+class MinecraftGPT(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
@@ -64,7 +46,23 @@ class Minecraft_GPT(nn.Module):
         self.drop_emb = nn.Dropout(cfg["drop_rate"])
         self.trf_blocks = nn.Sequential(
                 *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])]) 
-        #  ...
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.output_layer = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
 
     def forward(self, in_idx):
-        # under construction
+        batch_size = seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(
+                torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+torch.manual_seed(123)
+model = MinecraftGPT(cfg)
+
+total_params = sum(p.numel() for p in model.parameters())
+print(f"Total number of parameters: {total_params:,}")
--- models/transformerblock.py
+++ models/transformerblock.py
@@ -5,14 +5,18 @@ class TransformerBlock(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.att = MultiHeadAttention(
-                
-                )
+                    d_in = cfg["emb_dim"],
+                    d_out = cfg["emb_dim"],
+                    context_length = cfg["context_length"],
+                    num_heads = cfg["n_heads"],
+                    dropout = cfg["drop_rate"],
+                    qkv_bias = cfg["qkv_bias"])
         self.ff = FeedForward(cfg)
         self.norm1 = LayerNorm(cfg["emb_dim"])
         self.norm2 = LayerNorm(cfg["emb_dim"])
         self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
 
-    def forward(self, x)
+    def forward(self, x):
         shortcut = x
         x = self.norm1(x)
         x = self.att(x)
@@ -26,6 +30,56 @@ class TransformerBlock(nn.Module):
         x = x + shortcut
 
         return x
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert(d_out % num_heads == 0), \
+                "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+                "mask",
+                torch.triu(torch.ones(context_length, context_length), diagonal=1)
+                )
+    
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape # [batch, input_size, embedding_dim]
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        
+        queries = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+       
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        attn_scores = queries @ keys.transpose(2, 3)
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        context_vec = (attn_weights @ values).transpose(1, 2)
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec)
+        return context_vec
+
+
+
 
 
 class LayerNorm(nn.Module):
@@ -57,7 +111,7 @@ class FeedForward(nn.Module):
         self.layers = nn.Sequential(
                 nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
                 GELU(),
-                nn.Linear(4 * cfg["emb_dim"])
+                nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"])
                 )
 
     def forward(self, x):
