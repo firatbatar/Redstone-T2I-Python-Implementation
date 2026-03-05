ae0f4a17404da8ef33c690f023954a996d6263c9
EmreUte <emreute@gmail.com>
2026-03-05 17:26:14 +0300

    Implemented dataloder, requires input file which is in the making..

--- models/model.py
+++ models/model.py
@@ -1,10 +1,25 @@
 # Initialize the tokenizer
 import torch
 from tokenizer import MinecraftTokenizer
+from dataset_loader import MinecraftDataloader
+
 with open("vocab.txt", "r", encoding="utf-8") as f:
-    raw_text = f.read()
-
-all_words = raw_text.split('\n')[:-1]
+        words = f.read()
+all_words = words.split('\n')[:-1]
 vocab = {token:integer for integer, token in enumerate(all_words)}
-
 tokenizer = MinecraftTokenizer(vocab)
+
+dataloader = MinecraftDataloader(raw_text, 
+                                 tokenizer, 
+                                 batch_size=4, 
+                                 max_length=4, 
+                                 stride=2, 
+                                 shuffle=False)
+
+# test code
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
+
+
+
